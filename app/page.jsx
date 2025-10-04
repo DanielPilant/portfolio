638355fc922ce48f2cb9439de73d4c06b5797a0c
daniel pilant <doubledan148@gmail.com>
2025-10-05 01:40:36 +0300

    Implement code changes to enhance functionality and improve performance

---
+++ app/page.jsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// components
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
+import { Link } from "lucide-react";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none ">
+            <span>Software Developer</span>
+            <h1 className="mb-6 mt-4">
+              Hi, I&apos;m <br />{" "}
+              <span className="text-accent">Daniel Pilant</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white">
+              Software engineer focused on clean, impactful solutions. I build
+              apps, tools, and rendering engines that blend technical depth with
+              thoughtful design. As the founder of DevLev, I lead student
+              developers bridging academia and industry, while mentoring others
+              through Perach. My aim is to make technology both powerful and
+              human-centered.
+            </p>
+            {/* button and social links */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button asChild size="lg" className="uppercase flex items-center gap-2">
+                <a href="/assets/resume/Daniel_Pilant_resume.pdf" download="Daniel_Pilant_resume.pdf" target="_blank" rel="noopener noreferrer">
+                  <span>Download resume</span>
+                  <FiDownload className="text-xl" />
+                </a>
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border
+              border-accent rounded-full flex justify-center items-center
+              text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-8">
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Home;
